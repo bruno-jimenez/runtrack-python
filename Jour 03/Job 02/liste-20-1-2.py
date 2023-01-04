553df551dee27422ab0dc7ff8bb931d72a917416
unknown <bruno.jimenez@laplateforme.io>
2023-01-04 15:08:30 +0100

    adding job 3 and 4 files

--- Jour 03/Job 02/liste-20-1-2.py
+++ Jour 03/Job 02/liste-20-1-2.py
@@ -1,3 +1,11 @@
-for number in range(21):
-    if(number % 2):
-        print(number)
+# for number in range(21):
+#     if(number % 2):
+#        print(number)
+
+def loop():
+    i = 0
+    while i < 21:
+        print(i)
+        i += 2
+
+loop()
--- Jour 03/Job 03/liste-100.py
+++ Jour 03/Job 03/liste-100.py
@@ -1,7 +1,13 @@
-# for nbr in range(100):
-#    if (nbr % ):
-#        print(nbr % 27)
+def loop():
+    i = 0
+    while i < 100:
+        i += 1
+        if i == 26:
+            continue
+        if i == 37:
+            continue
+        if i == 88:
+            continue
+        print(i)
 
-for number in range(1001):
-    if(number  ):
-        print(number)
+loop()
